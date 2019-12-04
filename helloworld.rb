54f4db858307d0537f46810565da7cb9f0160710
masahiro <wktkmrnhrtmmshr@gmail.com>
2019-12-04 22:30:50 +0900

    abc141

--- helloworld.rb
+++ helloworld.rb
@@ -1,32 +1,68 @@
-N = 4
-S = "123123"
-
-ans = 0
-
-1000.times do |i|
-  num = "%03d" % i
-  tmp = S.dup
-  index = tmp.index(num[0])
-
-  if index == nil
-    next
-  else
-    tmp.slice!(index)
+class PriorityQueue
+  def initialize(&comp)
+    @heap = []
+    @node_num = 0
+    if !block_given?
+      @comp = Proc.new { |x, y| x <= y }
+    else
+      @comp = comp
+    end
   end
-  index = tmp.index(num[1])
-  if index == nil
-    next
-  else
-    tmp.slice!(index)
+
+  attr_reader :heap
+
+  def push(x)
+    current_node_num = @node_num
+    @node_num += 1
+    while current_node_num > 0
+      parent_node_num = (current_node_num - 1) / 2
+      break if @comp.call(@heap[parent_node_num], x) # 逆転してないなら抜ける
+      # 親ノードを下ろして自分を上に
+      @heap[current_node_num] = @heap[parent_node_num]
+      current_node_num = parent_node_num
+    end
+    @heap[current_node_num] = x
   end
-  index = tmp.index(num[2])
-  if index == nil
-    next
-  else
-    tmp.slice!(index)
+
+  alias_method(:<<, :push)
+
+  def pop
+    # 最(小|大)値
+    ret = @heap[0]
+    # 根にもってくる値
+    @node_num -= 1
+    x = @heap[@node_num]
+    # 根から下ろしていく
+    i = 0
+    while (a = i * 2 + 1) < @node_num
+      # 子どうしを比較
+      b = a + 1
+      a = b if b < @node_num && @comp.call(@heap[b], @heap[a])
+      break unless @comp.call(@heap[a], x)
+      @heap[i] = @heap[a]
+      i = a
+    end
+    @heap[i] = x
+    return ret
   end
-  puts num
-  ans += 1
+
+  def get
+    @node_num == 0 ? nil : @heap[0]
+  end
+
+  def empty?
+    return get.nil?
+  end
 end
 
-puts ans
+n, m = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i)
+q = PriorityQueue.new { |x, y| x > y }
+a.each do |e|
+  q.push e
+end
+
+m.times do
+  q.push(q.pop / 2)
+end
+puts q.heap.inject(:+)
